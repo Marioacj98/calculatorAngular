4ff137273de97b264490a2d13682a6f1b87189c9
Mario <marioacj98@gmail.com>
2020-05-05 10:47:23 -0500

    Some commets and good pratices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, HostListener  } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   valor = '0';
@@ -12,10 +12,10 @@ export class AppComponent implements OnInit {
   esperando = false;
   igualRealizado = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+  /* Keyboard use */
 
   @HostListener('window:keyup.1', ['$event']) n1(e: KeyboardEvent) {
     this.obtenerNumero('1');
@@ -63,59 +63,69 @@ export class AppComponent implements OnInit {
     this.realizarOperacion('=');
   }
   @HostListener('window:keyup', ['$event']) punto(e: KeyboardEvent) {
-   if (e.key === '.') {
-     this.decimal();
-   }
+    if (e.key === '.') {
+      this.decimal();
+    }
   }
   @HostListener('window:keyup.Backspace', ['$event']) reset(e: KeyboardEvent) {
     this.borrar();
   }
 
-
-  public obtenerNumero(v: string){
+/* Función para abstraer el número */
+  public obtenerNumero(v: string) {
     console.log(v);
-    if (this.esperando)
-    {
+    if (this.esperando) {
       if (this.igualRealizado) {
         this.borrar();
         this.valor = v;
-      }else{
+      } else {
         this.valor += v;
         this.esperando = false;
       }
-    }else{
-      this.valor === '0' ? this.valor = v : this.valor += v;
-
+    } else {
+      this.valor === '0' ? (this.valor = v) : (this.valor += v);
     }
   }
 
-  decimal(){
-    if (!this.valor.includes('.')){
-        this.valor += '.';
+  /* Función para validar los números decimales */
+  decimal() {
+    if (!this.valor.includes('.')) {
+      this.valor += '.';
     }
   }
 
-  private operacion(op , numero2){
-    switch (op){
+  /* Función para escoger el operador necesario */
+  private operacion(op, numero2) {
+    switch (op) {
       case '+':
-      return this.numero1 += numero2;
+        return (this.numero1 += numero2);
       case '-':
-      return this.numero1 -= numero2;
+        return (this.numero1 -= numero2);
       case '*':
-      return this.numero1 *= numero2;
+        return (this.numero1 *= numero2);
       case '/':
-      return this.numero1 /= numero2;
+        return (this.numero1 /= numero2);
       case '=':
-      return numero2;
+        return numero2;
     }
   }
-  public realizarOperacion(op: string){
-    if (this.numero1 === null){
+
+  /* Función para realizar la operación escogida */
+  public realizarOperacion(op: string) {
+    if (this.numero1 === null) {
       this.numero1 = Number(this.valor);
-    }else if (this.signo){
-      console.log(this.valor.substring(String(this.numero1).length + 1 ));
-      if (this.valor.includes('+') || this.valor.includes('-') || this.valor.includes('/') || this.valor.includes('*')) {
-        const resultado = this.operacion(this.signo , Number(this.valor.substring(String(this.numero1).length + 1 )));
+    } else if (this.signo) {
+      console.log(this.valor.substring(String(this.numero1).length + 1));
+      if (
+        this.valor.includes('+') ||
+        this.valor.includes('-') ||
+        this.valor.includes('/') ||
+        this.valor.includes('*')
+      ) {
+        const resultado = this.operacion(
+          this.signo,
+          Number(this.valor.substring(String(this.numero1).length + 1))
+        );
         this.valor = String(resultado);
         this.numero1 = resultado;
       }
@@ -123,17 +133,17 @@ export class AppComponent implements OnInit {
     this.signo = op;
     this.esperando = true;
 
-    if (this.signo !== '='){
+    if (this.signo !== '=') {
       this.valor += this.signo;
       this.igualRealizado = false;
-    }else{
+    } else {
       this.igualRealizado = true;
     }
     console.log(this.numero1);
-
   }
 
-  public borrar(){
+  /* Función para limpiar todo */
+  public borrar() {
     this.valor = '0';
     this.numero1 = null;
     this.signo = null;
